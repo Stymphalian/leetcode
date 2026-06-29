3049952bc1b5aa3413c81ddaef15b3c91bc189c1
Stymphalian <saturnslight@gmail.com>
2026-06-28 21:02:34 -0700

    p1967 - daily - 2026-06-28

---
+++ problems/p1967/main.cs
@@ -0,0 +1,5 @@
+// 1967 Number of Strings That Appear as Substrings in Word
+// https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/description/
+// Difficulty: Easy
+// Time Taken: HH:MM:SS
+
